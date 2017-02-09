5d9ffca4582c2d6222a38ef6893ed672d8cc556b
Scott Walker <scott.walker@digital.hmrc.gov.uk>
2017-02-09 09:40:59 +0000

    Test the transform logic by passing duplicate tracks for the same user

--- src/test/scala/spark/lastfm/Question1Spec.scala
+++ src/test/scala/spark/lastfm/Question1Spec.scala
@@ -8,21 +8,23 @@ class Question1Spec extends WordSpec with Matchers with BeforeAndAfter {
 
   "transform" should {
 
-    "return without duplicates" in {
+    "return without duplicate tracks" in {
       val userId1 = "some-user1"
       val timestamp1 = "2009-05-04T23:08:57Z"
       val artistId1 = "some-artistId1"
       val artistName1 = "some-artistName1"
       val trackId1: Option[String] = None
       val trackName1 = "some-trackName1"
+      val trackName2 = "some-trackName2"
       val user1Track1 = RecentTrack(userId1, timestamp1, artistId1, artistName1, trackId1, trackName1)
-      val recentTracks = Seq(user1Track1)
+      val user1Track2 = user1Track1.copy(trackName = trackName2)
+      val recentTracks = Seq(user1Track1, user1Track2, user1Track1, user1Track2, user1Track1, user1Track2)
 
       val stubbedContext = sc.parallelize[RecentTrack](recentTracks)
       val result = Question1.transform(stubbedContext).collect
 
       result should equal(Array(
-        s"$userId1\t1"
+        s"$userId1\t2"
       ))
     }
   }
